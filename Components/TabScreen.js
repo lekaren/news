7868342f179bed8dd92bfc6012d6e78bdf0e347f
Dabin <cpdnql@naver.com>
2020-09-11 11:35:48 +0900

    컴포넌트 수정

--- Components/TabScreen.js
+++ Components/TabScreen.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
-import { Container, Header, Content, Tab, Tabs, Title, Body } from 'native-base';
+import React from 'react';
+import { Container, Header, Tab, Tabs, Title, Body } from 'native-base';
 import TabOne from './Tabs/tabOne';
 import TabTwo from './Tabs/tabTwo';
-import tabThree from './Tabs/tabThree';
-export default class TabsExample extends Component {
-  render() {
-    return (
-      <Container>
-        <Header hasTabs>
-          <Body>
-            <Title>News App</Title>
-          </Body>
-        </Header>
-        <Tabs>
-          <Tab heading="Tab1">
-            <TabOne />
-          </Tab>
-          <Tab heading="Tab2">
-            <TabTwo />
-          </Tab>
-          <Tab heading="Tab3">
-            <tabThree />
-          </Tab>
-        </Tabs>
-      </Container>
-    );
-  }
+import TabThree from './Tabs/tabThree';
+
+function TabsExample (){
+  return (
+    <Container>
+      <Header hasTabs>
+        <Body>
+          <Title>News App</Title>
+        </Body>
+      </Header>
+      <Tabs>
+        <Tab heading="Tab1">
+          <TabOne />
+        </Tab>
+        <Tab heading="Tab2">
+          <TabTwo />
+        </Tab>
+        <Tab heading="Tab3">
+          <TabThree />
+        </Tab>
+      </Tabs>
+    </Container>
+  );
 }
+
+export default TabsExample
