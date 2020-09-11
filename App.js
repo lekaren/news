9a6fda01a955cd803952608060860c7838a57d1f
Dabin <cpdnql@naver.com>
2020-09-11 10:57:09 +0900

    폰트 문제 수정

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import TabScreen from './Components/TabScreen';
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import {Text} from "react-native"
 
 export default function App() {
-    return (
-      <TabScreen />
-    );
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const loadFont = async () => {
+      await Font.loadAsync({
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+        ...Ionicons.font,
+      });
+    };
+    loadFont().then(() => {
+      setLoading(false)
+    })
+  }, []);
+if (!loading) {
+  return (
+    <TabScreen />
+  );
+}else {
+  return <Text>에러</Text>
+  
+}
+    
   }
